8d5f9bbf02c209f6afd42192d77db3f93477d7ca
Alex Couture-Beil <alex@mofo.ca>
2013-10-20 23:21:18 -0700

    cmake cleanup

--- viewer_src/make_mocs.bat
+++
@@ -1,9 +0,0 @@
-c:\Qt\Qt5.0.2\5.0.2\msvc2010\bin\moc.exe task_entry_widget.h > _moc_task_entry_widget.cpp
-c:\Qt\Qt5.0.2\5.0.2\msvc2010\bin\moc.exe settings_dialog.h > _moc_settings_dialog.cpp
-c:\Qt\Qt5.0.2\5.0.2\msvc2010\bin\moc.exe user_settings.h > _moc_user_settings.cpp
-c:\Qt\Qt5.0.2\5.0.2\msvc2010\bin\moc.exe task_entry_window.h > _moc_task_entry_window.cpp
-c:\Qt\Qt5.0.2\5.0.2\msvc2010\bin\moc.exe logged_task_editor_dialog.h > _moc_logged_task_editor_dialog.cpp
-c:\Qt\Qt5.0.2\5.0.2\msvc2010\bin\moc.exe task_week_editor.h > _moc_task_week_editor.cpp
-c:\Qt\Qt5.0.2\5.0.2\msvc2010\bin\moc.exe editor_window.h > _moc_editor_window.cpp
-c:\Qt\Qt5.0.2\5.0.2\msvc2010\bin\moc.exe test_window.h > _moc_test_window.cpp
-pause
